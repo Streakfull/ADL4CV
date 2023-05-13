c50a517d54dcacda50438c396f5432c7d14907eb
Streakfull <youssef.youssef@tum.de>
2023-05-13 15:56:37 +0200

    modifies script

--- Project/src/DataSetPreprocessing/NrrdToMesh/nrrd_to_mesh.py
+++ Project/src/DataSetPreprocessing/NrrdToMesh/nrrd_to_mesh.py
@@ -3,7 +3,7 @@ import sys  # noqadefined
 import os  # noqa
 sys.path.append(os.path.abspath(os.path.join('..', '')))  # noqa
 from utils import read_nrrd, export_mesh_to_obj
-from constants import NRRD_EXTENSION, OBJ_EXTENTION, DEMO_ARGUMENT, DATA_SET_PATH, TEMP_PATH
+from constants import NRRD_EXTENSION, OBJ_EXTENTION, DEMO_ARGUMENT, DATA_SET_PATH, TEMP_PATH, ERRORS_PATH
 import skimage.measure as measure
 import numpy as np
 import tqdm
@@ -22,10 +22,16 @@ def nrrd_to_mesh(file_name):
     voxel_tensor = read_nrrd(file_name)
     alpha_channel = voxel_tensor[:, :, :, 3]
     binary_mask = (alpha_channel > 0).astype(np.uint8)
-    vertices, faces, _, _ = measure.marching_cubes(
-        binary_mask, allow_degenerate=True)
-    new_file_name = file_name.replace(NRRD_EXTENSION, OBJ_EXTENTION)
-    export_mesh_to_obj(new_file_name, vertices, faces)
+    try:
+        vertices, faces, _, _ = measure.marching_cubes(
+            binary_mask, allow_degenerate=True)
+        new_file_name = file_name.replace(NRRD_EXTENSION, OBJ_EXTENTION)
+        export_mesh_to_obj(new_file_name, vertices, faces)
+    except:
+        with open(ERRORS_PATH, "w") as file:
+            print("ERROR?")
+            file.write(file_name)
+            file.write('\n')
 
 
 def construct_full_nrrd_file_path(folder_name):
