46f808d18c4b20cb41f185ddae13577f3dbb956e
Streakfull <youssef.youssef@tum.de>
2023-05-24 18:08:40 +0200

    finalizes pr

--- Project/src/dataset_preprocessing/latent_code_extractor/extract_shape_codes.py
+++ Project/src/dataset_preprocessing/latent_code_extractor/extract_shape_codes.py
@@ -3,7 +3,7 @@ import sys
 import os
 import torch
 sys.path.append(os.path.abspath(os.path.join('..', '')))  # noqa
-from dataset_preprocessing.constants import DATA_SET_PATH, TEXT2SHAPE_DATA_SET_PATH,FULL_DATA_SET_PATH
+from dataset_preprocessing.constants import DATA_SET_PATH, TEXT2SHAPE_DATA_SET_PATH, FULL_DATA_SET_PATH
 from datasets.latent_code_extractor import LatentCodeExtractor
 from options.base_options import BaseOptions
 from encoder.pvqae import PVQVAE
@@ -16,10 +16,9 @@ import numpy as np
 import ast
 
 
-
 device = "cpu"
-# if (torch.cuda.is_available()):
-#     device = "cuda"
+if (torch.cuda.is_available()):
+    device = "cuda"
 root = ".."
 dataset_path = f"{root}/{DATA_SET_PATH}"
 text2Shape_dataset_path = f"{root}/{TEXT2SHAPE_DATA_SET_PATH}"
@@ -30,14 +29,14 @@ dataset = LatentCodeExtractor(
     text2Shape_dataset_path, dataset_path, cat=cat, resolution=64, load_similar_shapes=True)
 
 
-# vq_cfg = f"{root}/configs/pvqae_configs.yaml"
-# # Chckpoint here
-# chkpoint = f"{root}/{FULL_DATA_SET_PATH}/checkpoints/experiment_name/ckpt/vqvae_epoch-best.pth"
-# options = BaseOptions(config_path=vq_cfg,
-#                       name="pvqae-extraction", is_train=False)
-# model = PVQVAE()
-# model.initialize(options)
-# model.load_ckpt(chkpoint)
+vq_cfg = f"{root}/configs/pvqae_configs.yaml"
+# Chckpoint here
+chkpoint = f"{root}/{FULL_DATA_SET_PATH}/checkpoints/experiment_name/ckpt/vqvae_epoch-best.pth"
+options = BaseOptions(config_path=vq_cfg,
+                      name="pvqae-extraction", is_train=False)
+model = PVQVAE()
+model.initialize(options)
+model.load_ckpt(chkpoint)
 
 stats = {
     "shapes_dict": {},
@@ -45,7 +44,7 @@ stats = {
     "shape_set_counter": 0,
     "row_indices": {}
 }
-#codebook_indices = model.n_embed
+# codebook_indices = model.n_embed
 
 codebook_indices = 512
 n_cubes_per_dim = 8
@@ -65,8 +64,8 @@ def save_tensor(input, shape_id, is_shape_set=False, shape_set_row_index=0):
     if is_shape_set:
         file_path = f"{dataset_path}/{cat}/{shape_id}/z_set_{shape_set_row_index}.pt"
         # file_path = f"{dataset_path}/z_set_{shape_set_row_index}.pt"
-    with open(file_path,'wb') as f:
-      torch.save(input, f)
+    with open(file_path, 'wb') as f:
+        torch.save(input, f)
     print(colored(f'[*]{file_path} shapeset saved', 'yellow'))
 
 
@@ -77,8 +76,9 @@ def save_stats_dict():
         # Write the dictionary to the file in JSON format
         json.dump(stats, f)
 
-def save_dict(file_name,dictionary):
-   with open(f"{dataset_path}/{file_name}", 'w') as f:
+
+def save_dict(file_name, dictionary):
+    with open(f"{dataset_path}/{file_name}", 'w') as f:
         # Write the dictionary to the file in JSON format
         json.dump(dictionary, f)
 
@@ -92,7 +92,7 @@ def handle_shape(shape_id, shape_sdf):
     Saves:
         z tensor[ncubes_per_dim,ncubes_per_dim,ncubes_per_dim,n_embed ]: probability distribution over all possible codebook indices
     """
-    #is_shape_id_done = stats["shapes_dict"].get(shape_id, False)
+    # is_shape_id_done = stats["shapes_dict"].get(shape_id, False)
     # if (is_shape_id_done):
     #     return
     # 8 * 8 * 8
@@ -132,9 +132,6 @@ def handle_shape_set(scores, shape_id, shape_sets, shape_sets_indices):
         if (shapes_length > shapeset_length_threshold):
             z_set = handle_shape_set_sequentially(shape_set, scores=scores[i])
         else:
-            # indices = model.encode_indices(shape_set)
-            # z_set = functional.one_hot(indices, num_classes=codebook_indices)
-            # broadcasting for valid multiplication
             z_set = torch.tensor(shape_set).to(device)
             shape_set_scores = scores[i].reshape(
                 shape_set.shape[0], 1, 1, 1, 1).to(device)
@@ -150,64 +147,51 @@ def handle_shape_set(scores, shape_id, shape_sets, shape_sets_indices):
         stats["shape_set_counter"] += 1
         stats["row_indices"][shape_sets_indices[i]] = True
 
-#TODO: Batch process this
-def extract_z_shape():
-  for i in tqdm(range(4740,len(dataset))):
-    element = dataset[i]
-    shape_sdf = element["shape_sdf"]
-    # shape_sets = element["shape_sets_z"]
-    # shape_set_ids = element["shape_sets"]
-    # scores = element["shape_sets_scores"]
-    shape_id = element["shape_id"]
-    # shape_sets_indices = element["shape_sets_indices"]
+# TODO: Batch process this
 
-    handle_shape(shape_id, shape_sdf)
-    print(colored(f'[*]{shape_id} done', 'blue'))
-    #handle_shape_set(scores, shape_id, shape_sets, shape_sets_indices)
-    #print(colored(f'[*]{shape_id} shapeset done', 'blue'))
-    save_stats_dict()
+
+def extract_z_shape():
+    for i in tqdm(range(len(dataset))):
+        element = dataset[i]
+        shape_sdf = element["shape_sdf"]
+        shape_id = element["shape_id"]
+
+        handle_shape(shape_id, shape_sdf)
+        print(colored(f'[*]{shape_id} done', 'blue'))
+        save_stats_dict()
 
 
 def extract_z_shapesets():
-   for i in tqdm(range(len(dataset))):
-    element = dataset[i]
-    shape_sdf = element["shape_sdf"]
-    shape_sets = element["shape_sets_z"]
-    shape_set_ids = element["shape_sets"]
-    scores = element["shape_sets_scores"]
-    shape_id = element["shape_id"]
-    shape_sets_indices = element["shape_sets_indices"]
+    for i in tqdm(range(len(dataset))):
+        element = dataset[i]
+        shape_sets = element["shape_sets_z"]
+        scores = element["shape_sets_scores"]
+        shape_id = element["shape_id"]
+        shape_sets_indices = element["shape_sets_indices"]
+        handle_shape_set(scores, shape_id, shape_sets, shape_sets_indices)
+        print(colored(f'[*]{shape_id} shapeset done', 'blue'))
+        save_stats_dict()
 
-   # handle_shape(shape_id, shape_sdf)
-    #print(colored(f'[*]{shape_id} done', 'blue'))
-    handle_shape_set(scores, shape_id, shape_sets, shape_sets_indices)
-    print(colored(f'[*]{shape_id} shapeset done', 'blue'))
-    save_stats_dict()
 
 info = open(f"{dataset_path}/completed_set_8.json",)
 
 dictionary = json.load(info)
 
-print(len(dictionary.keys()))
-  
+
+print(colored(f'[*]{len(dictionary.keys())} shapes found', 'green'))
+
+
 def extract_z_shape_set_parallel(i):
     shape_id = dataset.get_id_from_index(i)
-    is_done = dictionary.get(shape_id,False)
-    if(is_done):
-      print(colored(f'[*]{shape_id} already done', 'green'))
-      return
+    is_done = dictionary.get(shape_id, False)
+    if (is_done):
+        print(colored(f'[*]{shape_id} already done', 'green'))
+        return
     element = dataset[i]
-  
     shape_id = element["shape_id"]
-    shape_sdf = element["shape_sdf"]
     shape_sets = element["shape_sets_z"]
-    shape_set_ids = element["shape_sets"]
     scores = element["shape_sets_scores"]
-
     shape_sets_indices = element["shape_sets_indices"]
-
-   # handle_shape(shape_id, shape_sdf)
-    #print(colored(f'[*]{shape_id} done', 'blue'))
     handle_shape_set(scores, shape_id, shape_sets, shape_sets_indices)
     print(colored(f'[*]{shape_id} shapeset done', 'blue'))
     stats["shapes_dict"][shape_id] = True
@@ -215,43 +199,25 @@ def extract_z_shape_set_parallel(i):
     save_stats_dict()
 
 
+def parallel_z_set():
+    indices = np.arange(len(dataset), dtype=int)
+    tokenizer = xmp.Vectorizer(extract_z_shape_set_parallel, num_workers=16)
+    tokenizer.process(indices)
 
-def parallel():
-  indices = np.arange(len(dataset),dtype=int)
-  tokenizer = xmp.Vectorizer(extract_z_shape_set_parallel,num_workers=16)
-  tokenizer.process(indices)
-  
-completed_shape_set ={
 
-}
+completed_shape_set = {}
 
-#count_done = 0
+
 def find_completed_shape_sets():
-  main_directory = f"{root}/{DATA_SET_PATH}/{cat}"
-  directories = os.listdir(main_directory)
-  print(len(directories))
-  for directory in directories:
-    files = os.listdir(f"{main_directory}/{directory}")
-    if(len(files)>=3):
-      completed_shape_set[directory] = True
-      #count_done +=1
-  save_dict("completed_set_8.json", completed_shape_set)
+    main_directory = f"{root}/{DATA_SET_PATH}/{cat}"
+    directories = os.listdir(main_directory)
+    print(len(directories))
+    for directory in directories:
+        files = os.listdir(f"{main_directory}/{directory}")
+        if (len(files) >= 3):
+            completed_shape_set[directory] = True
+    save_dict("completed_set_8.json", completed_shape_set)
 
 
-
-
-
-
-
-
-
-
-
-
-
-#print(count_done)
-
-#find_completed_shape_sets()
-
-parallel()
-
+# find_completed_shape_sets()
+# parallel_z_set()
